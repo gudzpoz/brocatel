dce505b415576aa771cfeefb153910942c719c06
gudzpoz <gudzpoz@live.com>
2023-08-24 20:49:48 +0800

    feat(mde): forbid directives in name fields

--- mde/src/directive-plugin.ts
+++ mde/src/directive-plugin.ts
@@ -1,6 +1,9 @@
 import { MilkdownPlugin } from '@milkdown/ctx';
-import { bulletListSchema, listItemSchema, paragraphSchema } from '@milkdown/preset-commonmark';
+import {
+  bulletListSchema, listItemSchema, paragraphSchema,
+} from '@milkdown/preset-commonmark';
 import { InputRule } from '@milkdown/prose/inputrules';
+import { TextSelection } from '@milkdown/prose/state';
 import {
   $inputRule, $nodeAttr, $nodeSchema, $remark,
 } from '@milkdown/utils';
@@ -10,14 +13,13 @@ import { plugins } from 'brocatel-mdc/src/index';
 export const remarkDirectivePlugin = $remark('remarkDirectivePlugin', () => plugins.remarkSimplifiedDirective as any);
 
 const DIRECTIVE_NAME_TYPE = 'directiveName';
-
 export const directiveNameAttr = $nodeAttr(DIRECTIVE_NAME_TYPE, () => ({ 'data-type': DIRECTIVE_NAME_TYPE }));
-
 export const directiveNameSchema = $nodeSchema(DIRECTIVE_NAME_TYPE, (ctx) => ({
-  content: 'paragraph',
+  content: 'text*',
   group: 'block',
+  defining: true,
   parseDOM: [{ tag: `p[data-type="${DIRECTIVE_NAME_TYPE}"]` }],
-  toDOM: (node) => ['div', ctx.get(directiveNameAttr.key)(node), 0],
+  toDOM: (node) => ['p', ctx.get(directiveNameAttr.key)(node), ['code', 0]],
   parseMarkdown: {
     match: () => false,
     runner: () => {},
@@ -27,10 +29,11 @@ export const directiveNameSchema = $nodeSchema(DIRECTIVE_NAME_TYPE, (ctx) => ({
     runner: (state, node) => {
       const top = state.top();
       if (top) {
+        const name = node.textContent || 'nil';
         if (top.props) {
-          top.props.name = node.textContent;
+          top.props.name = name;
         } else {
-          top.props = { name: node.textContent };
+          top.props = { name };
         }
       }
     },
@@ -38,19 +41,12 @@ export const directiveNameSchema = $nodeSchema(DIRECTIVE_NAME_TYPE, (ctx) => ({
 }));
 
 const DIRECTIVE_TYPE = 'containerDirective';
-
 export const directiveAttr = $nodeAttr(DIRECTIVE_TYPE, () => ({ 'data-type': DIRECTIVE_TYPE }));
-
 export const directiveSchema = $nodeSchema(DIRECTIVE_TYPE, (ctx) => ({
   content: `${DIRECTIVE_NAME_TYPE} bullet_list`,
   group: 'block',
   defining: true,
   atom: false,
-  attrs: {
-    name: {
-      default: 'name',
-    },
-  },
   parseDOM: [{
     tag: `div[data-type="${DIRECTIVE_TYPE}"]`,
   }],
@@ -62,9 +58,7 @@ export const directiveSchema = $nodeSchema(DIRECTIVE_TYPE, (ctx) => ({
       state
         .openNode(type, { name })
         .openNode(directiveNameSchema.type(ctx))
-        .openNode(paragraphSchema.type(ctx))
-        .addText(name || '')
-        .closeNode()
+        .addText(name || 'nil')
         .closeNode()
         .next(node.children)
         .closeNode();
@@ -83,20 +77,25 @@ export const directiveSchema = $nodeSchema(DIRECTIVE_TYPE, (ctx) => ({
 
 export const directiveInputRule = $inputRule((ctx) => new InputRule(
   /:::$/,
-  (state, _, start, end) => state.tr.replaceRangeWith(
-    start,
-    end,
-    directiveSchema.type(ctx).createChecked(null, [
-      directiveNameSchema.type(ctx).createChecked(null, [
-        paragraphSchema.type(ctx).createChecked(),
-      ]),
-      bulletListSchema.type(ctx).createChecked(null, [
-        listItemSchema.type(ctx).createChecked(null, [
-          paragraphSchema.type(ctx).createChecked(),
+  (state, _, start, end) => {
+    const pos = state.tr.doc.resolve(start);
+    if (pos.node(pos.depth - 1).type.name === DIRECTIVE_NAME_TYPE) {
+      return null;
+    }
+    const tr = state.tr.replaceRangeWith(
+      start,
+      end,
+      directiveSchema.type(ctx).createChecked(null, [
+        directiveNameSchema.type(ctx).createChecked(),
+        bulletListSchema.type(ctx).createChecked(null, [
+          listItemSchema.type(ctx).createChecked(null, [
+            paragraphSchema.type(ctx).createChecked(),
+          ]),
         ]),
       ]),
-    ]),
-  ),
+    );
+    return tr.setSelection(new TextSelection(tr.doc.resolve(start + 1)));
+  },
 ));
 
 export const directivePlugin: MilkdownPlugin[] = [
