cd996825bbd6863ba289cac9024edacc4056e713
gudzpoz <gudzpoz@live.com>
2023-10-19 18:24:16 +0800

    feat(mde): improve inline node editing

--- mde/src/nodes/mdx.ts
+++ mde/src/nodes/mdx.ts
@@ -1,9 +1,8 @@
 import { type MilkdownPlugin } from '@milkdown/ctx';
-import { toggleMark } from '@milkdown/prose/commands';
 import { InputRule } from '@milkdown/prose/inputrules';
 import {
   $command,
-  $inputRule, $markAttr, $markSchema, $remark,
+  $inputRule, $nodeAttr, $nodeSchema, $remark,
 } from '@milkdown/utils';
 
 import { mdxForMarkdown } from '@brocatel/md';
@@ -12,34 +11,35 @@ export const remarkMdxPlugin = $remark('remarkMdxPlugin', () => mdxForMarkdown);
 
 const MDX_AST_TYPE = 'mdxTextExpression';
 
-export const mdxInlineAttr = $markAttr(MDX_AST_TYPE);
+export const mdxInlineAttr = $nodeAttr(MDX_AST_TYPE, () => ({ 'data-type': MDX_AST_TYPE }));
 
-export const mdxInlineSchema = $markSchema(MDX_AST_TYPE, (ctx) => ({
+export const mdxInlineSchema = $nodeSchema(MDX_AST_TYPE, (ctx) => ({
+  content: 'text*',
+  group: 'inline',
+  inline: true,
   code: true,
-  inclusive: false,
-  parseDOM: [{
-    tag: `code[data-type="${MDX_AST_TYPE}"]`,
-  }],
-  toDOM: (mark) => [
-    'code',
-    {
-      'data-type': MDX_AST_TYPE,
-      ...ctx.get(mdxInlineAttr.key)(mark),
-    },
-  ],
+  isolating: true,
+  atom: false,
+  allowGapCursor: true,
+  parseDOM: [{ tag: `code[data-type="${MDX_AST_TYPE}"]` }],
+  toDOM: (node) => ['code', ctx.get(mdxInlineAttr.key)(node), 0],
   parseMarkdown: {
     match: (node) => node.type === MDX_AST_TYPE,
     runner: (state, node, type) => {
-      state
-        .openMark(type)
-        .addText(node.value as string)
-        .closeMark(type);
+      state.openNode(type);
+      const text = node.value as string;
+      if (text !== '') {
+        state.next({ type: 'text', value: text });
+      }
+      state.closeNode();
     },
   },
   toMarkdown: {
     match: (mark) => mark.type.name === MDX_AST_TYPE,
-    runner: (state, mark, node) => {
-      state.withMark(mark, MDX_AST_TYPE, node.text || '');
+    runner: (state, node) => {
+      state
+        .openNode(MDX_AST_TYPE, node.textContent)
+        .closeNode();
     },
   },
 }));
@@ -53,17 +53,17 @@ export const mdxInlineEscapeRule = [
   $inputRule(() => new InputRule(/\uFF5B[^{]*(\})$/, BIG_RIGHT_BRACE)),
 ];
 
-// Workaround: mdx at the beginning of line not recognized.
-export const mdxInlineInputRule = $inputRule((ctx) => new InputRule(
-  /^\{(.+)\}$/,
-  (state, _, start, end) => state.tr
-    .addMark(start + 1, end, mdxInlineSchema.type(ctx).create())
-    .delete(start, start + 1),
-));
-
 export const toggleMdxInlineCommand = $command(
   'toggleMdxInlineCommand',
-  (ctx) => () => toggleMark(mdxInlineSchema.type(ctx)),
+  (ctx) => () => (state, dispatch) => {
+    const { selection } = state;
+    const text = state.doc.cut(selection.from, selection.to).textContent;
+    const node = mdxInlineSchema.type(ctx).create(null, text ? state.schema.text(text) : null);
+    if (dispatch) {
+      dispatch(state.tr.replaceSelectionWith(node));
+    }
+    return true;
+  },
 );
 
 // We need no input rules since the inline-sync plugin does it for us.
@@ -72,7 +72,6 @@ export const mdxPlugin: MilkdownPlugin[] = [
   remarkMdxPlugin,
   mdxInlineAttr,
   mdxInlineEscapeRule,
-  mdxInlineInputRule,
   mdxInlineSchema,
   toggleMdxInlineCommand,
 ].flat();
